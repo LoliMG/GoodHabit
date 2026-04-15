8772b8065997f2617ebbb10ef3d900dcf768c191
Loli Mariscal <lolimg_jinete@hotmail.com>
2026-04-15 14:37:53 +0200

    Fix: Relative paths in PublicProfile.jsx

--- client/src/pages/Social/PublicProfile/PublicProfile.jsx
+++ client/src/pages/Social/PublicProfile/PublicProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { fetchData } from '../../helpers/axiosHelper';
-import UserProfileHeader from '../../components/User/UserProfileHeader/UserProfileHeader';
+import { fetchData } from '../../../helpers/axiosHelper';
+import UserProfileHeader from '../../../components/User/UserProfileHeader/UserProfileHeader';
 import './PublicProfile.css';
 
 const PublicProfile = () => {
